c0ae3ddbcf8667cac1861d260affadf413475d80
Neftalí Acosta <hola@neftaliacosta.com>
2016-05-04 01:44:40 -0500

    Versión 0.9.5

--- Calendario/fcalendario.cs
+++ Calendario/fcalendario.cs
@@ -14,23 +14,20 @@ namespace Calendario
             
             calendario get = new calendario();
             int mactual;
-            int contador=1;
-            int menor = 35;
-            
             string[] calen = new string[35];
             //int DateTime.DaysInMonth(int year, int month)
             int m;
-            int dactual, d, ndia, nyear,dinm;
-  
+            int dactual, d, ndia, nyear,dinm;//dactual>Obtiene el numero del nombre del dia de la semana
+            int w = 1; //Variable que escribe los días en el calendario
             mactual = DateTime.Now.Month;
+            //mactual = 4;
             dactual = DateTime.Now.Day;
-            nyear = DateTime.Now.Year;
-            ndia = DateTime.Now.Day;
+            nyear = DateTime.Now.Year; //Obtiene el año actual
+            ndia = DateTime.Now.Day; //Obtiene el día en fecha
+            int menor = 0;
             dinm = DateTime.DaysInMonth(nyear, mactual);
-            //DateTime fecha1 = new DateTime(nyear, DateTime.Now.Month, 1);
-            //DateTime fecha2 = new DateTime(nyear, DateTime.Now.Month+1, 1).AddDays(-1);
-            string dinicio = new DateTime(nyear, DateTime.Now.Month, 1).DayOfWeek.ToString();
-            string dfinal = new DateTime(nyear, DateTime.Now.Month + 1,1).AddDays(-1).DayOfWeek.ToString();
+            string dinicio = new DateTime(nyear, mactual, 1).DayOfWeek.ToString(); //Obtiene el dia de la semana que inicia el mes
+            string dfinal = new DateTime(nyear, mactual + 1,1).AddDays(-1).DayOfWeek.ToString();//Obtiene el dia de la semana que finaliza el mes
 
             dinicio =traductordias(dinicio);
             dfinal = traductordias(dfinal);
@@ -58,42 +55,43 @@ namespace Calendario
             get.mes[10] = "Nomviembre";
             get.mes[11] = "Diciembre";
 
-            int aux = 0;
-            for (int w=0; w<=34; w++)
+            
+            for (int var =0; var<=6; var++)
             {
-                if (dinicio==get.dia[w])
+                if (dinicio==get.dia[var])
                 {
-                    
-                    
-                    for ( aux=w; aux<dinm; aux++)
+                    for(int i=var; i <DateTime.DaysInMonth(nyear, mactual); i++)
                     {
-                        calen[aux] = contador.ToString();
-                        contador++;
+                        calen[i] = w.ToString();
+                        w++;
+                        
                     }
+                   
                 }
-                else if(aux <=menor)
+                for (menor=w-1; menor<=34; menor++)
                 {
-                    calen[aux] = "";
-                    aux++;
+                    calen[menor] = "  ";
                 }
                 
-
-
-                
-              
-                menor--;
             }
            
 
             Console.WriteLine("------------------------------------------------------");
-            Console.WriteLine("Hoy es "+ dactual+ " " + ndia+ " de " + get.mes[m] + " del " + nyear);
+            Console.WriteLine("Hoy es "+ get.dia[dactual]+ " " + ndia+ " de " + get.mes[m] + " del " + nyear);
             Console.WriteLine("\n");
             Console.WriteLine(get.dia[0]    +"|"+ get.dia[1]    +"|"+ get.dia[2]    +"|"+ get.dia[3]    +"|"+ get.dia[4]    +"|"+ get.dia[5]    +"|"+ get.dia[6]);
             Console.WriteLine("------------------------------------------------------");
-
-
-
-            System.Threading.Thread.Sleep(9000);
+            Console.WriteLine(calen[0]  +"      |"+ calen[1]  +"    |"+ calen[2]  +"     |"+ calen[3]  +"        |"+ calen[4]  +"     |"+ calen[5]  +"       |"+ calen[6]);
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(calen[7] + "      |" + calen[8] + "    |" + calen[9] + "    |" + calen[10] + "       |" + calen[11] + "    |" + calen[12] + "      |" + calen[13]);
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(calen[14] + "     |" + calen[15] + "   |" + calen[16] + "    |" + calen[17] + "       |" + calen[18] + "    |" + calen[19] + "      |" + calen[20]);
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(calen[21] + "     |" + calen[22] + "   |" + calen[23] + "    |" + calen[24] + "       |" + calen[25] + "    |" + calen[26] + "      |" + calen[27]);
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine(calen[28] + "     |" + calen[29] + "   |" + calen[30] + "    |" + calen[31] + "       |" + calen[32] + "    |" + calen[33] + "      |" + calen[34]);
+            //System.Threading.Thread.Sleep(9000);
+            Console.Read();
         }
 
         public static string traductordias(string dia)
